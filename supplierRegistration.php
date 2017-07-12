5e48d97c3f1be4f6c7427370812d6f624364121f
Chathuri <chathurimahanayake@gmail.com>
2017-07-12 17:57:46 +0530

    Created supplierRegistration page - Chathuri

---
+++ supplierRegistration.php
@@ -0,0 +1,19 @@
+
+<?php
+include './metaLibs.php';
+?>
+<title>Shehan Home</title>
+</head>
+<body>
+    <?php
+    include './pageheader.php';
+    ?>
+    
+    <div class="row">
+        <div class="col-lg-12 text-center h2">Supplier Regitration</div>
+    </div>
+    <div class="row">
+        
+    </div>
+</body>
+</html>
