f6daa7a5f2784fee1197fdc16b282aa624036f29
Chathuri <chathurimahanayake@gmail.com>
2017-07-12 16:39:59 +0530

    Initialize project and added page header- Chathuri

---
+++ metalibs.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/boot_style.css" rel="stylesheet">  
+        <link href="css/bootstrap.min.css" rel="stylesheet">  
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>  
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+
