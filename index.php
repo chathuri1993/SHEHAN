100d6f1cda6ca253cee82d1e8120b5d491725afd
Chathuri <chathurimahanayake@gmail.com>
2017-07-12 17:05:33 +0530

    Modified index page- Chathuri

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+
+<?php
+include './metaLibs.php';
+?>
+<title>Shehan Home</title>
+</head>
+<body>
+    <?php
+    include './pageheader.php';
+    ?>
+</body>
 </html>
--- pageheader.php
+++ pageheader.php
@@ -1,11 +1,4 @@
-
-<?php
-include './metaLibs.php';
-?>
-<title></title>
-</head>
-<body>
-    <div class="row">
+ <div class="row">
         <div class="col-lg-12 text-center"><img src="images/logo/shehan.svg"  width="300px" height="100px"/></div>
     </div>
     <nav class="navbar navbar-default">
@@ -18,28 +11,39 @@ include './metaLibs.php';
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#"><i class="fa fa-home" aria-hidden="true"></i>&nbsp;Home</a>
+                <a class="navbar-brand" href="index.php"><i class="fa fa-home" aria-hidden="true"></i>&nbsp;Home</a>
             </div>
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                    <li><a href="#">Link</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                        <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Supplier Management <span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li><a href="#">Action</a></li>
                             <li><a href="#">Another action</a></li>
                             <li><a href="#">Something else here</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">One more separated link</a></li>
                         </ul>
                     </li>
+                        <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Product Management <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                        <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Customer Management <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li ><a href="#">About Us<span class="sr-only">(current)</span></a></li>
+                
                 </ul>
             </div>
         </div>
     </nav>
-</body>
-</html>
+
